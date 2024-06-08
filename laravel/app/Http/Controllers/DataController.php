6b12a0c67f0dd956013df094a1640a97aad60ec6
Savindu <savinduruhunuhewa@gmail.com>
2024-06-08 07:45:39 +0530

    Edit the controller for reading the jason file

--- laravel/app/Http/Controllers/DataController.php
+++ laravel/app/Http/Controllers/DataController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class DataController extends Controller
 {
-    //
+    public function showData()
+    {
+        // Read the JSON file from storage/app directory
+        $json = Storage::get('data.json');
+
+        // Decode the JSON data into a PHP array
+        $data = json_decode($json, true);
+
+        // You can now use $data for further processing or return it as a response
+        return response()->json($data);
+    }
 }
