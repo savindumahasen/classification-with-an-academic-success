588023cc67fc54af8205eeda680287dc2f168bbe
Savindu <savinduruhunuhewa@gmail.com>
2024-06-08 17:50:17 +0530

    Create the logins table

--- laravel/database/migrations/2024_06_08_100826_create_logins_table.php
+++ laravel/database/migrations/2024_06_08_100826_create_logins_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('logins', function (Blueprint $table) {
             $table->id();
+            $table->string('username');
+            $table->string('password');
             $table->timestamps();
         });
     }
