c657ad4c7513b9d25738fa35c40f1fa2fdb79599
Savindu <savinduruhunuhewa@gmail.com>
2024-06-16 15:19:25 +0530

    Update the app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request, jsonify
-import joblib
+import pickle
 import numpy as np
-import pandas as pd
 
 app = Flask(__name__)
 
 # Load the model
-model = joblib.load('sample_submission_1.h5')
+with open('sample_submission_1.h5', 'rb') as model_file:
+    model = pickle.load(model_file)
 
 # Define the interpretation of prediction results
 def interpret_prediction(prediction):
@@ -26,18 +26,40 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Get data from request
-        input_data = [float(request.form.get(f'inputData{i}')) for i in range(1, 10)]
-        
-        # Perform prediction
-        prediction = model.predict(np.array(input_data).reshape(1, -1))[0]
-        
-        # Interpret prediction result
-        status = interpret_prediction(prediction)
+        data = request.get_json()
+        if not data:
+            return jsonify({'message': 'No input data provided'}), 400
 
-        return jsonify({'prediction': status}), 200
+        # Extract input data from JSON
+        input_data = [
+            data.get("inputData1"),
+            data.get("inputData2"),
+            data.get("inputData3"),
+            data.get("inputData4"),
+            data.get("inputData5"),
+            data.get("inputData6"),
+            data.get("inputData7"),
+            data.get("inputData8"),
+            data.get("inputData9")
+        ]
+
+        # Check for missing values
+        if None in input_data:
+            return jsonify({'message': 'Please enter numeric values for all fields.'}), 400
+
+        # Convert input data to numpy array
+        input_data = np.array([input_data], dtype=float)
+
+        # Make prediction
+        pred = model.predict(input_data)
+
+        # Interpret prediction result
+        prediction = interpret_prediction(pred[0])
+
+        return jsonify({'prediction': prediction}), 200
+
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({'message': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
